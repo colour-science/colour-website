0e098ab5e175a1ce1ddd5547c5385f3450438afe
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-09-19 21:05:09 +0200

    Add "About Rendering Engines Colourspaces Agnosticism" blog post skeleton.

---
+++ blog_about_rendering_engines_colourspaces_agnosticism.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <title>About Rendering Engines Colourspaces Agnosticism</title>
+
+    <?php include ("common_header_attributes.php"); ?>
+
+</head>
+
+<body>
+<?php include_once("analytics_tracking.php") ?>
+
+<?php include ("navigation.php"); ?>
+
+<!-- >>> Content -->
+<div class="container">
+    <div class="row">
+        <!-- >>> Center -->
+        <div class="col-md-9">
+            <div class="embed-responsive" style="height: 20550px;">
+                <iframe class="embed-responsive-item" src="ipython/about_rendering_engines_colourspaces_agnosticism.html"></iframe>
+            </div>
+
+            <?php include("disqus.php") ?>
+
+        </div>
+        <!-- <<< Center -->
+
+        <?php include ("sidebar.php"); ?>
+
+    </div>
+</div>
+<!-- <<< Content -->
+
+
+<?php include ("footer.php"); ?>
+
+<?php include ("common_body_attributes.php"); ?>
+
+</body>
+</html>
