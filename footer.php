363f3e7bb0790d17a0403ffde42ee227fa874dab
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-10-20 13:15:02 +0200

    Implement responsive footer.

--- footer.php
+++ footer.php
@@ -1,124 +1,113 @@
 <!-- >>> Footer -->
 <footer class="footer">
-    <div class="container">
+    <div class="container text-center">
         <div class="row">
-            <div class="col-md-2">
-                <a>About</a>
-                <ul class="list-footer">
-                    <li>
-                        <a href="contributors.php">Contributors</a>
-                    </li>
-                    <li>
-                        <a href="history.php">History</a>
-                    </li>
-                    <li>
-                        <?php include("contact.php") ?>
-                        <i class="fa fa-send"></i>&nbsp;Contact Us</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-2">
-                <a>Get Started</a>
-                <ul class="list-footer">
-                    <li>
-                        <a href="installation.php">Installation</a>
-                    </li>
-                    <li>
-                        <a href="tutorial.php">Tutorial</a>
-                    </li>
-                    <li>
-                        <a href="contributing.php"><span class="fa fa-gears">&nbsp;Contributing</span></a>
-                    </li>
-                    <li>
-                        <a href="faq.php">FAQ</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-2">
-                <a>Downloads</a>
-                <ul class="list-footer">
-                    <li>
-                        <a href="https://github.com/colour-science/colour/releases">All
-                            Releases</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/colour-science/colour"
-                           class="fa fa-github">&nbsp;Source Code</a>
-                    </li>
-                    <li>
-                        <a href="http://opensource.org/licenses/BSD-3-Clause">License</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-2">
-                <a class="fa fa-cube">&nbsp;Packages</a>
-                <ul class="list-footer">
-                    <li>
-                        <a href="colour_maya.php">Colour - Maya</a>
-                    </li>
-                    <li>
-                        <a href="colour_nuke.php">Colour - Nuke</a>
-                    </li>
-                    <li>
-                        <a href="colour_spectroscope.php">Colour -
-                            Spectroscope</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-2">
-                <a class="fa fa-life-ring">&nbsp;Documentation</a>
-                <ul class="list-footer">
-                    <li>
-                        <a href="api/latest/html/index.html"
-                           class="fa fa-life-ring">&nbsp;API Reference</a>
-                    </li>
-                    <li>
-                        <a href="http://nbviewer.ipython.org/github/colour-science/colour-ipython/blob/master/notebooks/colour.ipynb"
-                           class="fa fa-book">&nbsp;IPython Notebooks</a>
-                    </li>
-                    <li>
-                        <a href="contributing.php"><span class="fa fa-gears">&nbsp;Contributing</span></a>
-                    </li>
-                    <li>
-                        <a href="features.php" class="fa fa-lightbulb-o">&nbsp;Features</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-2">
-                <a><i class="fa fa-globe">&nbsp;News</i></a>
-                <ul class="list-footer">
-                    <li>
-                        <a href="news.php">Latest News</a>
-                    </li>
-                    <li>
-                        <a href="blog.php">Blog</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div id="footer-social">
-                    <ul class="list-inline">
+            <div class="col-lg-12">
+                <div class="col-md-2">
+                    <ul class="nav nav-pills nav-stacked">
                         <li>
-                            <?php include("contact.php") ?>
-                            <i class="fa fa-send fa-2x"></i></a>
+                            <strong>
+                                <span class="text-primary">About</span>
+                            </strong>
+                        </li>
+                        <li><a href="contributors.php">Contributors</a></li>
+                        <li><a href="history.php">History</a></li>
+                        <li><?php include("contact.php") ?>
+                            <i class="fa fa-send"></i>&nbsp;Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li>
+                            <strong>
+                                <span class="text-primary">Get Started</span>
+                            </strong>
+                        </li>
+                        <li><a href="installation.php">Installation</a></li>
+                        <li><a href="tutorial.php">Tutorial</a></li>
+                        <li><a href="contributing.php"><span
+                                class="fa fa-gears">&nbsp;Contributing</span></a>
+                        </li>
+                        <li><a href="faq.php">FAQ</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li>
+                            <strong>
+                                <span class="text-primary">Downloads</span>
+                            </strong>
                         </li>
                         <li>
-                            <a href="https://twitter.com/colour_science"><i
-                                    class="fa fa-twitter fa-2x"></i></a>
+                            <a href="https://github.com/colour-science/colour/releases">All
+                                Releases</a></li>
+                        <li><a href="https://github.com/colour-science/colour"
+                               class="fa fa-github">&nbsp;Source Code</a></li>
+                        <li>
+                            <a href="http://opensource.org/licenses/BSD-3-Clause">License</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li>
+                            <span class="fa fa-cube text-primary">&nbsp;<strong>Packages</strong></span>
+                        </li>
+                        <li><a href="colour_maya.php">Colour - Maya</a></li>
+                        <li><a href="colour_nuke.php">Colour - Nuke</a></li>
+                        <li><a href="colour_spectroscope.php">Colour -
+                            Spectroscope</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li>
+                            <span class="fa fa-globe text-primary">&nbsp;<strong>News</strong></span>
+                        </li>
+                        <li><a href="news.php">Latest News</a></li>
+                        <li><a href="blog.php">Blog</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li>
+                            <span class="fa fa-life-ring text-primary">&nbsp;<strong>Documentation</strong></span>
+                        </li>
+                        <li><a href="api/latest/html/index.html"
+                               class="fa fa-life-ring">&nbsp;API Reference</a>
                         </li>
                         <li>
-                            <a href="https://www.facebook.com/python.colour.science"><i
-                                    class="fa fa-facebook fa-2x"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://plus.google.com/u/0/b/101380914519940603334/101380914519940603334/about"><i
-                                    class="fa fa-google-plus fa-2x"></i></a>
+                            <a href="http://nbviewer.ipython.org/github/colour-science/colour-ipython/blob/master/notebooks/colour.ipynb"
+                               class="fa fa-book">&nbsp;IPython Notebooks</a>
                         </li>
                     </ul>
                 </div>
             </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4">
+                <ul class="nav nav-justified">
+                    <li><?php include("contact.php") ?>
+                        <i class="fa fa-send fa-2x"></i></a>
+                    </li>
+                    <li><a href="https://twitter.com/colour_science"><i
+                            class="fa fa-twitter fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/python.colour.science"><i
+                                class="fa fa-facebook fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com/u/0/b/101380914519940603334/101380914519940603334/about"><i
+                                class="fa fa-google-plus fa-2x"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4"></div>
         </div>
     </div>
 </footer>
