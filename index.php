aeccdf71f49117008649834804fdfd774f7c5823
Thomas Mansencal <thomas.mansencal@gmail.com>
2015-12-03 20:29:12 +1300

    Update landing page.

--- index.php
+++ index.php
@@ -4,22 +4,62 @@
     <meta charset="utf-8">
     <title>Colour: Colour Science for Python</title>
 
-    <?php include("common_header_attributes.php"); ?>
+    <?php include ("common_header_attributes.php"); ?>
+
+    <style>
+        #banner {
+            width: 100%;
+            min-width: 0px;
+            min-height: 0px;
+        }
+
+        #social {
+            padding-bottom: 2.5%;
+            padding-top: 2.5%;
+        }
+
+        .list-inline > li {
+            padding-left: 15px;
+            padding-right: 15px;
+        }
+    </style>
 </head>
 
 <body>
 <?php include_once("analytics_tracking.php") ?>
 
-<!-- >>> Content -->
+<div>
+    <div class="container">
+        <div class="row">
+
+            <div class="span12 text-center" id="social">
+                <ul class="list-inline">
+                    <li>
+                        <a href="mailto:colour-science@googlegroups.com"><i class="fa fa-send fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/colour_science"><i class="fa fa-twitter fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/python.colour.science"><i class="fa fa-facebook fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com/u/0/b/101380914519940603334/101380914519940603334/about"><i class="fa fa-google-plus fa-2x"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <img src="images/Colour_Logo_Banner_Soon_001.png" id="banner"/>
+</div>
+
 <div class="container">
     <div class="row">
-        <img src="images/Colour_Logo_Banner_Soon_001.png" class="img-responsive">
+        <!-- >>> Center -->
+        <div class="col-md-12">
+            <?php include("disqus.php") ?>
+        </div>
     </div>
 </div>
-<!-- <<< Content -->
-
-
-<?php include("common_body_attributes.php"); ?>
-
 </body>
 </html>
